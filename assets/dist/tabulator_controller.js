4774a2a42c5d9c4858991fa5a1d08a68692d13f0
Igor Chunikhin <6insanes@gmail.com>
2024-08-22 14:15:50 +0300

    Do not morph table just refresh

--- assets/dist/tabulator_controller.js
+++ assets/dist/tabulator_controller.js
@@ -3,41 +3,49 @@ import { TabulatorFull } from "tabulator-tables"
 
 export default class TabulatorController extends Controller {
 
-  static values = {
-    options: Object
-  }
-
-  connect() {
-    const options = {
-      ...this.optionsValue,
-      locale: true,
-      langs: {
-        "ru": {
-          "pagination": {
-            "page_size": "Выводить по",
-            "first": "В начало",
-            "first_title": "Перейти к первой странице",
-            "last": "В конец",
-            "last_title": "Перейти к последней странице",
-            "prev": "Назад",
-            "prev_title": "Перейти на предыдущую страницу",
-            "next": "Вперед",
-            "next_title": "Перейти на следующую страницу",
-            "all": "Все",
-          },
-        },
-      },
-      paginationCounter: function (pageSize, currentRow, currentPage, totalRows, totalPages) {
-        const currentPageTotalRows = totalPages === currentPage ? totalRows : currentPage * pageSize
-        return `Просмотр ${currentRow} - ${currentPageTotalRows} из ${totalRows}`
-      }
+    static values = {
+        options: Object
     }
 
-    this.dispatch('pre-connect', { detail: { options } })
-    this.tabulator = new TabulatorFull(this.element, options)
-  }
+    connect() {
+        const options = {
+            ...this.optionsValue,
+            locale: true,
+            langs: {
+                "ru": {
+                    "pagination": {
+                        "page_size": "Выводить по",
+                        "first": "В начало",
+                        "first_title": "Перейти к первой странице",
+                        "last": "В конец",
+                        "last_title": "Перейти к последней странице",
+                        "prev": "Назад",
+                        "prev_title": "Перейти на предыдущую страницу",
+                        "next": "Вперед",
+                        "next_title": "Перейти на следующую страницу",
+                        "all": "Все",
+                    },
+                },
+            },
+            paginationCounter: function (pageSize, currentRow, currentPage, totalRows, totalPages) {
+                const currentPageTotalRows = totalPages === currentPage ? totalRows : currentPage * pageSize
+                return `Просмотр ${currentRow} - ${currentPageTotalRows} из ${totalRows}`
+            }
+        }
 
-  disconnect() {
-    this.tabulator.destroy()
-  }
+        this.dispatch('pre-connect', {detail: {options}})
+        this.tabulator = new TabulatorFull(this.element, options)
+
+        this._beforeMorphElement = (event) => {
+            event.preventDefault()
+            this.tabulator.setData()
+        }
+
+        this.element.addEventListener('turbo:before-morph-element', this._beforeMorphElement)
+    }
+
+    disconnect() {
+        this.tabulator.destroy()
+        this.element.removeEventListener('turbo:before-morph-element', this._beforeMorphElement)
+    }
 }

cc4f9b8f8ff4c7b8cc0386e84a5023bb5117840e
Igor Chunikhin <6insanes@gmail.com>
2024-08-30 15:28:30 +0300

    Extract locators to separate services

--- config/services.php
+++ config/services.php
@@ -8,6 +8,7 @@ use DeviantLab\TabulatorBundle\Controller\TableController;
 use DeviantLab\TabulatorBundle\TableFactory;
 use DeviantLab\TabulatorBundle\TableTwigExtension;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\DependencyInjection\ServiceLocator;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -17,15 +18,23 @@ return function(ContainerConfigurator $container): void {
     $services->set(TableTwigExtension::class)
         ->tag('twig.extension');
 
+    $services->set('deviantlab.tabulator.table_type_locator', ServiceLocator::class)
+        ->args([tagged_iterator('deviantlab.tabulator.table_type')])
+        ->tag('container.service_locator');
+
+    $services->set('deviantlab.tabulator.table_type_locator_by_name', ServiceLocator::class)
+        ->args([tagged_iterator('deviantlab.tabulator.table_type', defaultIndexMethod: 'getName')])
+        ->tag('container.service_locator');
+
     $services->set(TableFactory::class)
         ->args([
             service(UrlGeneratorInterface::class),
-            tagged_locator('deviantlab.tabulator.table_type')
+            service('deviantlab.tabulator.table_type_locator'),
         ]);
 
     $services->set(TableController::class)
         ->args([
-            tagged_locator('deviantlab.tabulator.table_type', defaultIndexMethod: 'getName'),
+            service('deviantlab.tabulator.table_type_locator_by_name'),
             service(ManagerRegistry::class),
             service(SerializerInterface::class),
         ])
